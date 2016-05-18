08c77cb6193e414901af06dc71555e4a7357e22f
jdiamond <jonathan@swiftnav.com>
2016-05-18 15:33:42 -0700

    added scripts for testing ascii sample grabber output

--- quick_grab/acq_test.py
+++ quick_grab/acq_test.py
@@ -2,7 +2,10 @@ import peregrine.samples
 import sys
 import peregrine.analysis.acquisition
 import peregrine.log; peregrine.log.docs_logging_config()
+import numpy as np
 samps = peregrine.samples.load_samples(sys.argv[1])
+print len(samps)
+print np.unique(samps)
 acq = peregrine.acquisition.Acquisition(samps, 16.368e6, 4.092e6, 16368)
 res = acq.acquisition(show_progress=False); res
 
--- quick_grab/gen_samples.py
+++ quick_grab/gen_samples.py
@@ -6,28 +6,32 @@ import peregrine.samples
 
 dataFile = sys.argv[1]
 
-data=np.array([]);
+
 
 with open(dataFile, 'r') as f:
 	content = f.readlines()
 
 last = 100
-lineCount=1
+lineCount=0
+data=np.zeros(len(content),dtype=np.int8);
 for line in content:
 	val = int(line,16)
-	check=val>>8
-	if not last==100 and not last == check:
+	check=val>>4
+	if not last==100 and not (last+1)%13 == check:
+		print(val,check,last)
 		print("check failed",lineCount)
 		exit(1)
 	last = check
-	data=np.append(data,[val&0xF])
-	if not data[-1]==0 and not data[-1]==1:
+	data[lineCount]=val&0xF
+	if not data[lineCount]==0 and not data[lineCount]==1:
 		print("bad data",lineCount)
 		exit(1)
-	if data[-1]==0:
-		data[-1]=-1
+	if data[lineCount]==0:
+		data[lineCount]=-1
 	lineCount=lineCount+1
 
-peregrine.samples.save_samples("samples_file.dat", data)
+data=data[0:len(data)/8*8]
+
+peregrine.samples.save_samples("samples_file.piksi_format", data,file_format='piksi')
 
 
